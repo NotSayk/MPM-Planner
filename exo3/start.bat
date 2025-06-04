30429a37b25d2a801fdfef022cffd6f2986d4c34
NotSayk <samuampe@gmail.com>
2025-06-04 14:01:34 +0200

    ajout batc

--- exo3/start.bat
+++ exo3/start.bat
@@ -1,6 +1,5 @@
-@echo off
 javac @compile.list -d bin >nul 2>&1
-cp src/liseTache.txt bin >nul 2>&1
+copy listeTache.txt bin/listeTache.txt >nul 2>&1
 cd bin >nul 2>&1
 java src.Controleur
 cd .. >nul 2>&1
