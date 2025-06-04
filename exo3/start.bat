dd5cab341c4f8be2954c78a2424820a34a26b283
NotSayk <samuampe@gmail.com>
2025-06-04 14:04:09 +0200

    ajout

--- exo3/start.bat
+++ exo3/start.bat
@@ -1,7 +1,22 @@
 @echo off
-javac @compile.list -d bin >nul 2>&1
-copy listeTache.txt bin/listeTache.txt >nul 2>&1
-cd bin >nul 2>&1
-java src.Controleur
-cd .. >nul 2>&1
+setlocal
+
+REM Check for Mac OS (Unix-like)
+if "%OS%"=="Windows_NT" (
+    REM Windows commands
+    javac @compile.list -d bin >nul 2>&1
+    copy listeTache.txt bin/listeTache.txt >nul 2>&1
+    cd bin >nul 2>&1
+    java src.Controleur
+    cd .. >nul 2>&1
+) else (
+    REM Mac/Unix commands
+    javac @compile.list -d bin >/dev/null 2>&1
+    cp listeTache.txt bin/listeTache.txt >/dev/null 2>&1
+    cd bin >/dev/null 2>&1
+    java src.Controleur
+    cd .. >/dev/null 2>&1
+)
+
 pause
+endlocal
