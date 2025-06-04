a9fc4d863a87897f1b3e7fafa2563211de5d3b8e
Ethan Damestoy <cytelid@gmail.com>
2025-06-04 09:28:21 +0200

    z

---
+++ exo3/src/utils/DateUtils.java
@@ -0,0 +1,34 @@
+package src.utils;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class DateUtils 
+{
+    
+    public static String getDateDuJour() 
+    {
+        GregorianCalendar calendar = new GregorianCalendar()            ;
+        int               jour     = calendar.get(Calendar.DAY_OF_MONTH);
+        int               mois     = calendar.get(Calendar.MONTH) + 1   ;
+        int               annee    = calendar.get(Calendar.YEAR)        ;
+        
+        return String.format("%02d/%02d/%04d", jour, mois, annee);
+    }
+
+    public static String ajouterJourDate(String date, int jours) 
+    {
+        String[] parties = date.split("/")             ;
+        int      jour    = Integer.parseInt(parties[0]);
+        int      mois    = Integer.parseInt(parties[1]);
+        int      annee   = Integer.parseInt(parties[2]);
+
+        GregorianCalendar calendar = new GregorianCalendar(annee, mois - 1, jour);
+        calendar.add(Calendar.DAY_OF_MONTH, jours)                               ;
+
+        return String.format("%02d/%02d/%04d", calendar.get(Calendar.DAY_OF_MONTH), 
+                                                calendar.get(Calendar.MONTH) + 1  , 
+                                                calendar.get(Calendar.YEAR))      ;
+    }
+
+}
