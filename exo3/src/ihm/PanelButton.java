eb51e9faea02312fcdc475226e88c41b582cf9c0
Ethan Damestoy <cytelid@gmail.com>
2025-06-10 11:25:32 +0200

    Update PanelButton.java

--- exo3/src/ihm/PanelButton.java
+++ exo3/src/ihm/PanelButton.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.*;
-
+import javax.swing.border.Border;
 import src.Controleur;
 import src.utils.ErrorUtils;
 
